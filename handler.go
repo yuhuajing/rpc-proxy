42265e1d81a7d871860a518a5c5a75d17c25de42
yuhuajing <18852897525@163.com>
2023-11-10 15:42:14 +0800

    env

--- handler.go
+++ handler.go
@@ -6,7 +6,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"math/big"
 	"net"
 	"net/http"
 	"strings"
@@ -94,7 +93,7 @@ func parseRequests(r *http.Request) (string, []string, []ModifiedRequest, error)
 			return "", nil, nil, err
 		}
 		toAddr := tx.To()
-		signer := types.NewLondonSigner(big.NewInt(ChainID))
+		signer := types.LatestSignerForChainID(tx.ChainId())
 		sender, _ := signer.Sender(tx)
 		senderAddr := strings.ToLower(sender.Hex())
 		if toAddr != nil {
