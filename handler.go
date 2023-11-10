c7c277e26c8dbda9bf999d8ac89738e50a54b932
yuhuajing <18852897525@163.com>
2023-11-10 15:19:09 +0800

    update

--- handler.go
+++ handler.go
@@ -102,7 +102,6 @@ func parseRequests(r *http.Request) (string, []string, []ModifiedRequest, error)
 			fmt.Println(SCAddress[strings.ToLower(toAddr.Hex())])
 		} else {
 			fmt.Println(senderAddr)
-			fmt.Println(toAddr.Hex())
 			if !SCAddress[senderAddr] {
 				//if senderAddr != "0x60a6e5af0525523a617cf6c1f85353fba0408a7b" && senderAddr != "0x68d866baafa993bc002cd35218c13f10ac54221d" && senderAddr != "0xdd15a18b453eb92140a149f774d1c792919bb352" {
 				return "", nil, nil, fmt.Errorf("NOT_APPROVED_DEPLOY_CONTRACT")
